07bdea110e9f050836957b162b5103e7821370e0
MuhammadSabih70 <154405586+MuhammadSabih70@users.noreply.github.com>
2024-03-15 03:32:52 +0500

    Add files via upload

---
+++ index.js
@@ -0,0 +1,22 @@
+// var student = ["nadeem","hamza","muzammil","furqan",890,"ali"]
+// console.log(student[4])
+// student[student.length] = "cam";
+
+// student.push(false)
+// console.log(student)
+
+var q = ["karachi","islamabad","lahore","multan","peshawar","quetta"]
+var w = q.slice(2,4)
+console.log(q)
+console.log(w)
+
+var e = prompt("enter your message here")
+var r = e.split(" ")
+console.log(e)
+console.log(r)
+
+var t = ["this","is","a","pool","to","swim"]
+var y = t.join(" ")
+console.log(t)
+console.log(y)
+
