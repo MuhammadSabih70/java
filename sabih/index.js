8b6a5c19a3997ce5e92fe005fec92b7f7047ab4d
MuhammadSabih70 <154405586+MuhammadSabih70@users.noreply.github.com>
2024-03-22 07:14:54 +0500

    Add files via upload

---
+++ sabih/index.js
@@ -0,0 +1,44 @@
+// var ih = " "
+// for ( i=0; i<10; i++){
+//     ih=ih+ "*"
+// console.log(ih)}
+
+// var gh = ["karachi","hyedrabad","peshawar","quetta","islamabad"]
+// var user= prompt ("enter your city")
+// var isfound = false
+// for (var i=0; i<gh.length; i++){
+//     if(user==gh[i]){console.log("city is found" +user)}
+//     isfound=true   
+// }
+// if(isfound === false){console.log("not found")}
+
+var k = [[]]
+var j = [
+    ["0","1","2","3"+ "<br>"],["5","4","6","7"+ "<br>"],["5","2","3","6"]
+] 
+document.write(j)
+
+var i = ""
+for (i=1;i<11; i ++){
+    document.write(i+"<br>")
+}
+
+var hu = prompt("enter your number")
+var t = prompt("enter your lenght")
+for (let ti=1;ti<=t; ti ++ ){
+    document.write(hu + "x" + ti +"=" + hu*t + "<br>")
+}
+
+var fruits = ["apple","banana","pineapple","mango","orange"]
+for (var i=0;i<fruits.length;i++){
+    document.write(fruits[i]+"<br>")
+}
+for (var i=0;i<=20;i++){
+    if(i%2==0){document.write(i+"<br>") }
+}
+for (var i=0;i<=20;i++){
+    if(i%2 !==0){document.write(i+"<br>") }
+}
+for (var z=10;z>0;z--){
+    document.write(z + "<br>")
+}
