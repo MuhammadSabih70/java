1cd302c6a8a70744736ca24e88b1aad052af1550
MuhammadSabih70 <154405586+MuhammadSabih70@users.noreply.github.com>
2024-03-29 07:11:58 +0500

    Add files via upload
    Signed-off-by: MuhammadSabih70 <154405586+MuhammadSabih70@users.noreply.github.com>

--- index.js
+++ index.js
@@ -1,22 +1,72 @@
-// var student = ["nadeem","hamza","muzammil","furqan",890,"ali"]
-// console.log(student[4])
-// student[student.length] = "cam";
+// var ih = " "
+// for ( i=0; i<10; i++){
+//     ih=ih+ "*"
+// console.log(ih)}
 
-// student.push(false)
-// console.log(student)
+// var gh = ["karachi","hyedrabad","peshawar","quetta","islamabad"]
+// var user= prompt ("enter your city")
+// var isfound = false
+// for (var i=0; i<gh.length; i++){
+//     if(user==gh[i]){console.log("city is found" +user)}
+//     isfound=true   
+// }
+// if(isfound === false){console.log("not found")}
 
-var q = ["karachi","islamabad","lahore","multan","peshawar","quetta"]
-var w = q.slice(2,4)
-console.log(q)
-console.log(w)
+// var k = [[]]
+// var j = [
+//     ["0","1","2","3"+ "<br>"],["5","4","6","7"+ "<br>"],["5","2","3","6"]
+// ] 
+// document.write(j)
 
-var e = prompt("enter your message here")
-var r = e.split(" ")
-console.log(e)
-console.log(r)
+// var i = ""
+// for (i=1;i<11; i ++){
+//     document.write(i+"<br>")
+// }
 
-var t = ["this","is","a","pool","to","swim"]
-var y = t.join(" ")
-console.log(t)
-console.log(y)
+// var hu = prompt("enter your number")
+// var t = prompt("enter your lenght")
+// for (let ti=1;ti<=t; ti ++ ){
+//     document.write(hu + "x" + ti +"=" + hu*t + "<br>")
+// }
+
+// var fruits = ["apple","banana","pineapple","mango","orange"]
+// for (var i=0;i<fruits.length;i++){
+//     document.write(fruits[i]+"<br>")
+// }
+// for (var i=0;i<=20;i++){
+//     if(i%2==0){document.write(i+"<br>") }
+// }
+// for (var i=0;i<=20;i++){
+//     if(i%2 !==0){document.write(i+"<br>") }
+// }
+// for (var z=10;z>0;z--){
+//     document.write(z + "<br>")
+// }
+// for (var i=1;i<6;i++ ){
+//    document.write(i*i + "<br>") 
+// }
+
+// var userIn = prompt("enetre your word here to chek it is pallindrome")
+// var isItPalLindroMe = true 
+
+// for (var i=0; i<userIn.lenght;i++){
+//     var start = userIn[i]
+//     var end = userIn[(userIn-i)-i]
+//     console.log(start)
+//     console.log(end)
+//     if ( start =! end ){
+//         isItPalLindroMe = false
+//     }
+// }
+// if (isItPalLindroMe){
+// alert("it is pallindrome")
+// }
+// else{
+//     alert("it is not pallindrome")
+// }
+
+
+
+
+
 
